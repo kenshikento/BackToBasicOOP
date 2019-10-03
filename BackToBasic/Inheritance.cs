7e34f8d9a30317f7fa78e50ad7baf9764c2da8a5
kenshikento <tien-nguyen1@hotmail.co.uk>
2019-10-03 20:59:18 +0100

    'test'

---
+++ BackToBasic/Inheritance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackToBasic
+{
+    class Inheritance
+    {
+        public String apples()
+        {
+            return "apples";
+        }
+    }
+}
