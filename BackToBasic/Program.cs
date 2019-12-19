44e44a260de5a688abf4f156f18770c062060c0d
Nguyen <tien-nguyen1@hotmail.co.uk>
2019-12-19 14:19:11 +0000

    added abstraction but also console commands for it

--- BackToBasic/Program.cs
+++ BackToBasic/Program.cs
@@ -6,60 +6,84 @@ namespace BackToBasic
     {
         static void Main(string[] args)
         {
-            // Encapsulation 
-            Inheritance test = new Inheritance();
-            Console.WriteLine("Inheritance Result :"+ test.controller());   
-
-            // Encapsulation
-            Encapsulation test1 = new Encapsulation();
-            Console.WriteLine("Encapuslation Result :"+ test1.controllerEncap());
-
-            // Polymorphism  
-
-            // Method Overloading 
-            Teacher test2 = new Teacher();
-            Console.WriteLine("Encapuslation Result :"+ test2.controllerMethodOverloading());
-            
-            // Operator overloading 
-            BinaryOperator test3 = new BinaryOperator();
-            Console.WriteLine("Binary Result :"+ test3.returnBinaryOverloading());
-
-            UnaryOperator test4 = new UnaryOperator();
-            Console.WriteLine("UnaryOperator Result :"+ test4.returnUnaryOperator());
-
-            // Dynamic Polymorphism
-            Chef test5 = new Chef();
-            Console.WriteLine("Base Class");
-            test5.GetAge();
-            test5.GetName();
-
-            HeadChef test6 = new HeadChef();
-            Console.WriteLine("Run time poly");
-            test6.GetAge();
-            test6.GetName();
-
-
-
-            // Abstraction 
-
-
-
-            /*if(args.Length == 0)
+            if(args.Length == 0)
             {   
                 Console.WriteLine("Enter Something in");
             }
+            
+            string[] arrayCheckList = {"Inheritance","Encapsulation","MethodOverloading","BinaryOperator","UnaryOperator","Dynamic","Abstraction"};
 
             if(args.Length > 0)
-            {
+            {   
                 foreach (string value in args) 
                 {
+                    if(Array.Exists(arrayCheckList, element => element != value)) 
+                    {   
+                        Console.WriteLine("Doesn't match please type in one of these [Inheritance,Encapsulation,MethodOverloading,BinaryOperator,UnaryOperator,Dynamic,Abstraction]");
+                    }
+
                     if(value == "Inheritance") 
-                    {
+                    {   //Inheritance
                         Inheritance test = new Inheritance();
                         Console.WriteLine("Inheritance Result :" + test.controller() + args);
                     }
+
+                    if(value == "Encapsulation") 
+                    {
+                        // Encapsulation
+                        Encapsulation test1 = new Encapsulation();
+                        Console.WriteLine("Encapuslation Result :"+ test1.controllerEncap());
+                    }
+
+                    // Polymorphism
+                    if(value == "MethodOverloading") 
+                    {
+                        // Method Overloading 
+                        Teacher test2 = new Teacher();
+                        Console.WriteLine("Encapuslation Result :"+ test2.controllerMethodOverloading());
+                    }
+
+                    // Operator overloading 
+                    if(value == "BinaryOperator") 
+                    {   
+                        //Binary Operator
+                        BinaryOperator test3 = new BinaryOperator();
+                        Console.WriteLine("Binary Result :"+ test3.returnBinaryOverloading());
+                    }
+
+                    // Operator overloading 
+                    if(value == "UnaryOperator") 
+                    {
+                        //UnaryOperator
+                        UnaryOperator test4 = new UnaryOperator();
+                        Console.WriteLine("UnaryOperator Result :"+ test4.returnUnaryOperator());
+                    }
+
+                    // Dynamic Polymorphism
+                    if(value == "Dynamic") 
+                    {
+                        // Base
+                        Chef test5 = new Chef();
+                        Console.WriteLine("Base Class");
+                        test5.GetAge();
+                        test5.GetName();
+
+                        // Run Time Poly
+                        HeadChef test6 = new HeadChef();
+                        Console.WriteLine("Run time poly");
+                        test6.GetAge();
+                        test6.GetName();
+                    }
+
+                    // Abstraction
+                    if(value == "Abstraction") 
+                    {
+                        //UnaryOperator
+                        Abstraction test7 =  new Abstraction();
+                        Console.WriteLine("Abstraction Result :"+ test7.controllerAbstract());
+                    }
                 }
-            }*/
+            }
         }
     }
 }
